461ed74cd3e3a302a1ad034730f5b9d6e5690cc7
adfaure <adrien.faure2@gmail.com>
2017-10-12 23:28:33 +0200

    add emacs file: copied from Arnaud repo https://raw.githubusercontent.com/alegrand/RR_webinars/master/1_replicable_article_laboratory_notebook/init.el

--- install.sh
+++ install.sh
@@ -8,10 +8,13 @@
 #
 # empty line are not allowed, howerver it is possible to use
 # variable such as $HOME. If you the script as sudoer, please remenber to use `sudo -E ` so the environement is kept.
-
 MAPPING=`cat mapping.txt`
 backup_dir="./backup"
 
+# Funciton that takes two parameters.
+# The first parameter is the name of the file from this repository files/$1.
+# The second parameter is the path where the file should be installed in the system.
+# Described in the mapping.txt "$1=$2"
 function instal_file {
   if [ -f $2 ]; then
     bak_file=./backup/$(basename $1).bak
@@ -29,15 +32,21 @@ function instal_file {
   ln -sf `pwd`/files/$1 $2
 }
 
+# Creating a foder to backup files
+# before replacing it.
 if ! [ -d $backup_dir ]; then
   mkdir $backup_dir
 fi
 
+# This loop iterates over all
+# the mappiing.txt file and install every files.
 for map in $MAPPING
 do
   echo ""
-  path=$(eval "echo ${map#*=}")
+  # Extracting the part before the equal
   file=${map%=*}
+  # Extracting the part after the equal
+  path=$(eval "echo ${map#*=}")
   instal_file $file $path
   echo ""
 done
